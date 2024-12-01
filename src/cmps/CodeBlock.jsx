9f2bfe26f3220037ef854f8c3856802ae0b249d7
Yiftach Samocha <yiftachsamo@gmail.com>
2024-12-01 15:40:28 +0200

    Add basic socket behavior in editing blocks

--- src/cmps/CodeBlock.jsx
+++ src/cmps/CodeBlock.jsx
@@ -1,26 +1,44 @@
 import { useEffect, useState } from "react"
 import { loadBlock, updateBlock } from "../store/actions/block.actions"
 import { useSelector } from "react-redux"
+import { SOCKET_EMIT_EDIT_BLOCK, SOCKET_EMIT_SET_BLOCK_TYPE, SOCKET_EVENT_BLOCK_EDITED, socketService } from "../services/socket.service"
 
 export function CodeBlock({ type }) {
     const [content, setContent] = useState('')
     const block = useSelector(state => state.blockModule.currBlock)
 
     useEffect(() => {
+        socketService.on(SOCKET_EVENT_BLOCK_EDITED, editContent)
+        return () => {
+            socketService.off(SOCKET_EVENT_BLOCK_EDITED, editContent)
+        }
+    }, [type])
+
+    useEffect(() => {
         loadBlock(type)
             .then(block => {
                 setContent(block.content)
-            })
-    }, [])
 
-    async function editText({ target }) {
-        const { value } = target
-        setContent(value)
-        const blockToUpdate = { ...block, content: value }
+            })
+    }, [type])
+
+    useEffect(() => {
+        socketService.emit(SOCKET_EMIT_SET_BLOCK_TYPE, type)
+    }, [type])
+
+    async function editContent(editedContent) {
+        setContent(editedContent)
+        const blockToUpdate = { ...block, content: editedContent }
         await updateBlock(blockToUpdate)
     }
 
+    function handleChange({ target }) {
+        const { value } = target
+        editContent(value)
+        socketService.emit(SOCKET_EMIT_EDIT_BLOCK, value)
+    }
+
     return <section className="code-block">
-        <textarea value={content} onChange={editText}></textarea>
+        <textarea value={content} onChange={handleChange}></textarea>
     </section>
 }
