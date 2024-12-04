4c21eeaa0a31ca3b6ceae4b68869fc020ffd8158
Yiftach Samocha <yiftachsamo@gmail.com>
2024-12-04 17:15:58 +0200

    Fix bug

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Routes, Route, useNavigate, useLocation} from 'react-router'
+import { Routes, Route, useNavigate, useLocation } from 'react-router'
 import { AppHeader } from './cmps/AppHeader'
 import { CodeIndex } from './pages/CodeIndex'
 import { Lobby } from './pages/Lobby'
-import { SOCKET_EMIT_GET_BLOCK_TYPE, SOCKET_EVENT_BAD_CONNECTION, SOCKET_EVENT_SET_BLOCK_TYPE, SOCKET_EVENT_SET_CURR_USER, SOCKET_EVENT_SET_USERS_AMOUNT, socketService } from './services/socket.service'
+import { SOCKET_EMIT_GET_BLOCK_TYPE, SOCKET_EMIT_GET_CURR_USER, SOCKET_EMIT_GET_USERS_AMOUNT, SOCKET_EVENT_BAD_CONNECTION, SOCKET_EVENT_SET_BLOCK_TYPE, SOCKET_EVENT_SET_CURR_USER, SOCKET_EVENT_SET_USERS_AMOUNT, socketService } from './services/socket.service'
 import { useDispatch } from 'react-redux'
 import { SET_CURR_USER, SET_USERS_AMOUNT } from './store/block.reducer'
 import { QuestionMsg } from './cmps/msgs-cmps/QuestionMsg'
@@ -14,7 +14,7 @@ import { loadBlock } from './store/block.actions'
 export function RootCmp() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const location= useLocation()
+    const location = useLocation()
     const [currQuestion, setCurrQuestion] = useState(null)
 
     useEffect(() => {
@@ -34,6 +34,8 @@ export function RootCmp() {
     useEffect(() => {
         let type = getCurrentTypeFromPath()
         socketService.emit(SOCKET_EMIT_GET_BLOCK_TYPE, type)
+        socketService.emit(SOCKET_EMIT_GET_CURR_USER, null)
+        socketService.emit(SOCKET_EMIT_GET_USERS_AMOUNT, null)
     }, [location])
 
 
--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,13 +1,16 @@
 import io from 'socket.io-client'
+export const SOCKET_EMIT_GET_BLOCK_TYPE = 'get-block-type'
+export const SOCKET_EMIT_GET_CURR_USER = 'get-curr-user'
+export const SOCKET_EMIT_GET_USERS_AMOUNT= 'get-users-amount'
 export const SOCKET_EMIT_EDIT_BLOCK = 'edit-block'
 export const SOCKET_EMIT_SEND_QUESTION = 'send-question'
 export const SOCKET_EMIT_SEND_ANSWER = 'send-answer'
-export const SOCKET_EMIT_GET_BLOCK_TYPE = 'get-block-type'
 
-export const SOCKET_EVENT_BLOCK_EDITED = 'block-edited'
+
 export const SOCKET_EVENT_SET_BLOCK_TYPE = 'set-block-type'
 export const SOCKET_EVENT_SET_CURR_USER = 'set-curr-user'
 export const SOCKET_EVENT_SET_USERS_AMOUNT = 'set-users-amount'
+export const SOCKET_EVENT_BLOCK_EDITED = 'block-edited'
 export const SOCKET_EVENT_GET_QUESTION = 'get-question'
 export const SOCKET_EVENT_GET_ANSWER = 'get-answer'
 export const SOCKET_EVENT_BAD_CONNECTION = 'bad-connection'
